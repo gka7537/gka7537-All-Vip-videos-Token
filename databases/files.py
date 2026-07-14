87353709b2a2278c05a907451fc040f8f92a9c68
gka7537 <gka7537@gmail.com>
2026-07-14 17:35:06 +0530

    Update files.py

--- databases/files.py
+++ databases/files.py
@@ -1,1 +1,12 @@
+from database import db
 
+async def save_file(file_data, media_group_id=None):
+    # Agar media_group_id hai, to ek hi album mein save karein
+    data = {
+        "file_id": file_data.file_id,
+        "name": file_data.file_name,
+        "size": file_data.file_size,
+        "media_group_id": media_group_id  # Ye album ke liye hai
+    }
+    await db.files.insert_one(data)
+  
