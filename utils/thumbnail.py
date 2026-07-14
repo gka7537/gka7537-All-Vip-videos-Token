f11abb99615c69d2d5df11c51787e1893a1043db
gka7537 <gka7537@gmail.com>
2026-07-14 17:35:39 +0530

    Update thumbnail.py

--- utils/thumbnail.py
+++ utils/thumbnail.py
@@ -1,1 +1,10 @@
+import cv2
 
+def get_thumbnail(video_path, output_path):
+    cap = cv2.VideoCapture(video_path)
+    success, image = cap.read()
+    if success:
+        cv2.imwrite(output_path, image)
+    cap.release()
+    return output_path
+  
