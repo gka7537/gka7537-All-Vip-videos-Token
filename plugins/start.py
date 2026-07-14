2ff830e97953008e998e01c416ef5c242f50f2be
gka7537 <gka7537@gmail.com>
2026-07-14 19:27:23 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters, types
 @Client.on_message(filters.command("start"))
 async def start_handler(client, message):
     await message.reply(
-        "Bot chal raha hai! Verification ke liye click karein...",
+        "Welcome to my bot!",
         reply_markup=types.InlineKeyboardMarkup([[
             types.InlineKeyboardButton("Verify Now", callback_data="verify_now")
         ]])
