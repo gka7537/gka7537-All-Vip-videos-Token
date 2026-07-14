54505c88efde79f4b8769740cb4e89e373bbb2d3
gka7537 <gka7537@gmail.com>
2026-07-14 17:42:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+import os
+
+# Render se variable read karne ka code
+API_ID = int(os.environ.get("API_ID", 0))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+MONGO_DB_URL = os.environ.get("MONGO_DB_URL", "")
+# Agar ek se zyada admin hain, to comma lagayein (e.g., 12345,67890)
+ADMINS = [int(x) for x in os.environ.get("ADMINS", "0").split(",")]
+PORT = int(os.environ.get("PORT", 10000))
 
