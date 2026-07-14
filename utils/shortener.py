be82aa3cd77f5b7bc8780625147ef93a7940ecae
gka7537 <gka7537@gmail.com>
2026-07-14 17:13:14 +0530

    Update shortener.py

--- utils/shortener.py
+++ utils/shortener.py
@@ -1,1 +1,11 @@
+import aiohttp
 
+async def get_shortlink(url):
+    api_url = "YOUR_SHORTLINK_API_URL" # Jaise Ouo.io, Exe.io etc.
+    api_key = "YOUR_API_KEY"
+    
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"{api_url}?api={api_key}&url={url}") as resp:
+            data = await resp.json()
+            return data.get("shortenedUrl", url) # Error par original url return hoga
+          
